1699e15b7a0cc1c07aa2dbde4beb58b732dea74e
miguegallo88ss <miguegallo88@hotmail.com>
2018-11-21 18:23:56 -0600

    Adding divisible by 3 or 5 exercise

---
+++ problems/sum_of_natural_nums_devisible_by_3_and_5/sum_of_natural_nums_devisible_by_3_and_5/sum_of_natural_nums_devisible_by_3_and_5.cpp
@@ -0,0 +1,43 @@
+// sum_of_natural_nums_devisible_by_3_and_5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 
+//Description
+//the user neter a natural number and the pgm computes sum of all number from 
+// zeto to taht number, that are divisible by 3 & 5
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int in_num = 0;
+	int loop_ctr = 0;
+	int sum = 0;
+
+
+	cout << " \n Please enter a number: \n";
+	cin >> in_num;
+
+	for ( loop_ctr = 3; loop_ctr <= in_num; loop_ctr++) {
+		if ((loop_ctr % 5 == 0) || (loop_ctr % 3 == 0)) {
+			sum = loop_ctr + sum;
+			cout << "Divisible by Either " << loop_ctr << "\n";
+		}
+	}
+
+    cout << "\nThe sum is " << sum << "\n"; 
+
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
