331e00bb45bb70b0c20e20b39ca4a6fd34cf3411
miguegallo88ss <miguegallo88@hotmail.com>
2018-11-23 21:40:11 -0600

    Adding projrct intended to explains arrays a little : exercise/managing_arrays_and_strings/

---
+++ exercise/managing_arrays_and_strings/managing_arrays_and_strings/managing_arrays_and_strings.cpp
@@ -0,0 +1,20 @@
+// managing_arrays_and_strings.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Doing some Array[] Stuff!\n"; 
+
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
